f909a4a8b78d52858eaec36c08e2d94f6f664950
nynfus <99591020+nynfus@users.noreply.github.com>
2023-04-07 16:16:12 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'FriscoEgyreKenyérebb',
   tagline: 'Avagy mi mindenre jó a kovász',
   url: 'https://nynfus.github.io',
-  baseUrl: '/kovasz',
+  baseUrl: '/kovasz/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
